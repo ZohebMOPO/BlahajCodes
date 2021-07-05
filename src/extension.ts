ba63d35cd0af7b8620b964802481fcda6c2156c5
Zoheb Alli Khan <understandingzak@gmail.com>
2021-07-05 08:58:11 +0530

    Some features

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,34 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { PrideBlahaj } from "./PrideBlahaj";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "blahajcodes" is now active!');
+  console.log("Blahaj is ON!");
+  context.subscriptions.push(
+    vscode.commands.registerCommand("blahajcodes.web", async () => {
+      PrideBlahaj.createOrShow(context.extensionUri);
+    })
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand("blahajcodes.Pride", async () => {
+      const answer = await vscode.window.showInformationMessage(
+        "how was your day with INIT",
+        "Awesome",
+        "Tiring"
+      );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('blahajcodes.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from BlahajCode!');
-	});
-
-	context.subscriptions.push(disposable);
+      if (answer === "Awesome") {
+        vscode.window.showInformationMessage("Blahaj Loves you");
+      } else {
+        vscode.window.showInformationMessage(
+          "Join BlahajGang. You will not feel tired"
+        );
+      }
+    })
+  );
 }
 
 // this method is called when your extension is deactivated
